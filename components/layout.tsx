6c166071bb1e4574443bfa3be47a8d60bd1e23cc
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-01 20:31:22 +0500

    h

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,8 +8,8 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
     <div className=" h-screen  bg-black">
-        <div className=" container mx-auto max-w-6xl xl:px-30 bg-green-300">
-            <div className=" grid grid-cols-4 h-full  bg-red-300">
+        <div className=" container mx-auto max-w-6xl xl:px-30 h-full">
+            <div className=" grid grid-cols-4 h-full  ">
                 <div className=" col-span-3 lg:col-span-2 border-x-2 border-neutral-800">
     
         {children}

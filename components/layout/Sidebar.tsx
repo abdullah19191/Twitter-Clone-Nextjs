97126c44eac6920eaaf6c91edc81d435e789dbc2
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-02 01:06:11 +0500

    Adding sidebar items

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BsHouseFill,BsBellFill} from 'react-icons/bs'
 import {FaUser} from 'react-icons/fa'
 import SidebarLogo from './Sidebarlogo'
+
 const Sidebar = () => {
      const items = [
         {
@@ -28,6 +29,14 @@ const Sidebar = () => {
             <div className="">
                 <div className="">
                     <SidebarLogo/>
+                    {items.map((items) => (
+                        <SidebarItem
+                        key={items.href}
+                        href={items.href}
+                        label={items.label}
+                        icon={items.icon}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
--- components/layout/Sidebarlogo.tsx
+++ components/layout/Sidebarlogo.tsx
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+import { BsTwitter } from "react-icons/bs";
+
 const SidebarLogo =() => {
+    const router = useRouter()
+    
     return (
-        <div className="">
-            
+        <div onClick={() => router.push('/')} className=" flex justify-center items-center h-14 w-14 transition rounded-full p-4 hover:bg-blue-300 hover:bg-opacity-10  cursor-pointer">
+           <BsTwitter color="white" size={30}/>
         </div>
     );
 }

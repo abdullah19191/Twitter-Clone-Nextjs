41f3453d222a34047368d794d53b4e8bbcf9b0e1
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-01 23:44:13 +0500

    rt

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,25 +1,23 @@
- 
+
 import React from 'react'
 
 interface LayoutProps {
     children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({children}) => {
-  return (
-    <div className=" h-screen  bg-black">
-        <div className=" container mx-auto max-w-6xl xl:px-30 h-full">
-            <div className=" grid grid-cols-4 h-full  ">
-                <div className=" col-span-3 lg:col-span-2 border-x-2 border-neutral-800">
-    
-        {children}
-     
-
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className=" h-screen  bg-black">
+            <div className=" container mx-auto max-w-6xl xl:px-30 h-full">
+                <div className=" grid grid-cols-4 h-full  ">
+                    
+                    <div className=" col-span-3 lg:col-span-2 border-x-2 border-neutral-800">
+                        {children}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Layout

36cc5e4e72e000315e6b1992d20b9f42e06f6b7d
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-02 18:20:38 +0500

    tweet

--- components/layout/Sidebar/SidebarTweetButton.tsx
+++ components/layout/Sidebar/SidebarTweetButton.tsx
@@ -1,8 +1,14 @@
+import { useRouter } from 'next/router'
 import React from 'react'
-
+import {FaFeather} from 'react-icons/fa'
 const SidebarTweetButton = () =>{
+    const router = useRouter()
   return (
-    <div>SidebarTweetButton</div>
+    <div onClick={()=> router.push('/')}> 
+<div className=" rounded-full bg-blue-400 flex items-center justify-center w-14 h-14">
+    <FaFeather size={24} color='white'/>
+</div>
+    </div>
   )
 }
 

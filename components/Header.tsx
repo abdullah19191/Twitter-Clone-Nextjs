f98bbf11898e2769411076ffbf528fa10c141b40
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-03 00:24:11 +0500

    arrow back added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useCallback } from 'react'
+import { BiArrowBack } from 'react-icons/bi';
 
+interface HeaderProps {
+    label:string;
+    showBackArrow?:boolean;
+}
 
-const Header = () => {
+const Header:React.FC<HeaderProps> = ({label, showBackArrow}) => {
+    const router = useRouter();
+    
+    const handleBack = useCallback(()=>{
+        router.back()
+    }, [router])
   return (
-    <div>Header</div>
+    <div>
+        <div className="">
+            <div className="">
+                {
+                    showBackArrow && (
+                        <BiArrowBack
+                        onClick={handleBack}
+                        color='white'
+                        size={20}
+                        className=" cursor-pointer transition hover:opacity-70"
+                        />
+                    )
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 

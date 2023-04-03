db37ce2a7ed236c2ed97e6c1fdf2c1e71483acdb
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-03 18:38:23 +0500

    reusable

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,41 +1,41 @@
-import React from 'react'
+import React from "react";
 
 interface ButtonProps {
-    label:string;
-    secondary?:boolean;
-    fullWidth?:boolean;
-    large?:boolean;
-    onClick:()=> void;
-    disbled?:boolean;
-    outline?:boolean;
+  label: string;
+  secondary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  onClick: () => void;
+  disbled?: boolean;
+  outline?: boolean;
 }
 
 const Buttons: React.FC<ButtonProps> = ({
-    label,
-    secondary,
-    fullWidth,
-    large,
-    onClick,
-    disbled,
-    outline,
+  label,
+  secondary,
+  fullWidth,
+  large,
+  onClick,
+  disbled,
+  outline,
 }) => {
   return (
-    <button className={` disabled:opacity-70 
+    <button
+      className={` disabled:opacity-70 
     disabled:cursor-not-allowed 
     rounded-full
      font-semibold
       hover:opacity-80
       transition
        border-2
-       ${fullWidth ? 'w-full': 'w-fit'}
-       ${secondary ? ' bg-white': ' bg-sky-500'}
-       ${secondary ? '  text-black': '  text-white'}
-       ${secondary ? ' border-black': ' border-sky-500'}
+       ${fullWidth ? "w-full" : "w-fit"}
+       ${secondary ? " bg-white" : " bg-sky-500"}
+       ${secondary ? "  text-black" : "  text-white"}
+       ${secondary ? " border-black" : " border-sky-500"}
       
-    `}>
+    `}
+    ></button>
+  );
+};
 
-    </button>
-  )
-}
-
-export default Buttons
+export default Buttons;

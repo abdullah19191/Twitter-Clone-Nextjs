0b233746dc75647ab2d3cebf4b8a9ee713f60a59
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-04 00:32:52 +0500

    st

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const Model = () => {
+interface ModelProps {
+    isOpen?:boolean;
+    onClose:() => void;
+    onSubmit: () => void;
+    title?:string;
+    body?:React.ReactElement;
+    footer?:React.ReactElement;
+    actionLabel:string;
+    disabled?:boolean;
+}
+
+const Model:React.FC<ModelProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+}
+) => {
   return (
     <div>
-        
+
     </div>
   )
 }

4779c6212c292da96f175231279927c91d5d39b5
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-04 01:27:31 +0500

    chg

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react";
-
+import {AiOutlineClose} from "react-icons/ai"
 interface ModelProps {
   isOpen?: boolean;
   onClose: () => void;
@@ -43,9 +43,14 @@ const Model: React.FC<ModelProps> = ({
       <div
         className=" justify-center flex items-center bg-neutral-800 bg-opacity-70 outline-none overflow-x-hidden overflow-y-auto z-50 fixed inset-0"
       >
-        <div className=" relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full bg-red-800 lg:h-auto">
-            <div className=" h-full lg:h-auto border-0 rounded-lg shadow-lg relative flex  flex-col w-full  outline-none focus:outline-none">
-                <div className=" flex items-center justify-between p-10 rounded-t "></div>
+        <div className=" relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full lg:h-auto">
+            <div className=" h-full lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black  outline-none focus:outline-none">
+                <div className=" flex items-center justify-between p-10 rounded-t">
+                    <h3 className=" text-3xl font-semibold text-white" >{title}</h3>
+                    <button onClick={handleClose} className=" p-1 ml-auto border-0 text-white hover:opacity-60  transition">
+                        <AiOutlineClose color="white" size={20}/>
+                        </button>
+                </div>
             </div>
         </div>
       </div>

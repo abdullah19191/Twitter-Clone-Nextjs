b265ecc57b72b4b5ec3d8f2012abe8f8512b6dd1
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-04 14:49:36 +0500

    Input reusable widget

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React from "react";
 
-
- interface InputProps {
-    placeholder?:string;
-    value?:string;
-    type?:string;
-    disabled?:boolean;
-    onCharge:(event:React.ChangeEvent<HTMLInputElement>) => void;
-
- }
-
-const Input:React.FC<InputProps> = ({
-    placeholder,
-    value,
-    type,
-    disabled,
-    onCharge,
-}) => {
-  return (
-    <div>Input</div>
-  )
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+  type?: string;
+  disabled?: boolean;
+  onCharge: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default Input
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  value,
+  type,
+  disabled,
+  onCharge,
+}) => {
+  return (
+    <input
+      disabled={disabled}
+      onChange={onCharge}
+      value={value}
+      placeholder={placeholder}
+      type={type}
+      className="
+     w-full
+     p-4  
+     text-lg
+     bg-black
+     border-2 
+      border-neutral-800
+     rounded-md 
+     outline-none
+     text-white
+     focus:border-sky-500
+      focus:border-2
+      transition
+       disabled:bg-neutral-50
+        disabled:opacity-70
+         disabled:cursor-not-allowed
+     "
+    />
+  );
+};
+
+export default Input;

af84d4d6aa38685b9f3e1e62dfa98f4a38695b0f
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-04 15:23:06 +0500

    added

---
+++ components/Models/LoganModel.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const  LoganModel =() =>{
+  return (
+    <div>LoganModel</div>
+  )
+}
+
+export default LoganModel
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import LoganModel from "../components/Models/LoganModel";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <LoganModel/>
       <Layout>
         <Component {...pageProps} />
       </Layout>

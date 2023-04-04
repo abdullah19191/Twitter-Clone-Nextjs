e3b3dafa163e2ea9e9ee1990c01f2f1333f9e9af
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-04 23:08:29 +0500

    LoganModel finished

--- components/Models/LoganModel.tsx
+++ components/Models/LoganModel.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useState } from "react";
 import useLoganModal from "../../Hooks/useLoginModel";
+import Input from "../Input";
+import Model from "../Model";
 
 const LoganModel = () => {
   const loginModel = useLoganModal();
@@ -18,7 +20,24 @@ const LoganModel = () => {
       setIsLoading(false);
     }
   }, [loginModel]);
-  return <div>LoganModel</div>;
+
+   const bodyContent = (
+    <div className=" flex flex-col gap-4">
+        <Input value={email} disabled={isLoading} placeholder="Email" onCharge={(e) => setEmail(e.target.value)}/>
+        <Input value={password} disabled={isLoading} placeholder="Password" onCharge={(e) => setEmail(e.target.value)}/>
+    </div>
+   )
+  return (
+    <Model
+     disabled={isLoading}
+     isOpen={loginModel.isOpen}
+     title="Login"
+     actionLabel="Sign In"
+     onClose={loginModel.onClose}
+     onSubmit={onSubmit}
+     body={bodyContent}
+    />
+  )
 };
 
 export default LoganModel;

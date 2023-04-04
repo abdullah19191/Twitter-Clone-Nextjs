36ce271e90bb9be2d755cfb184892a521d1d3998
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-04 23:01:23 +0500

    changes

--- components/Models/LoganModel.tsx
+++ components/Models/LoganModel.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import useLoganModal from "../../Hooks/useLoginModel";
 
+const LoganModel = () => {
+  const loginModel = useLoganModal();
 
-const  LoganModel =() =>{
-  return (
-    <div>LoganModel</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-export default LoganModel
+  const onSubmit = useCallback(  async () => {
+    try {
+      setIsLoading(true);
+      loginModel.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [loginModel]);
+  return <div>LoganModel</div>;
+};
+
+export default LoganModel;

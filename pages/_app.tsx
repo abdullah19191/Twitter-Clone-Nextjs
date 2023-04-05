ca5777ae7b8e4386de48b7060eba88f89258ccdf
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-06 00:06:08 +0500

    add regiter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import LoganModel from "../components/Models/LoganModel";
+import RegisterModel from "../components/Models/RegisterModel";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <RegisterModel />
       <LoganModel />
       <Layout>
         <Component {...pageProps} />

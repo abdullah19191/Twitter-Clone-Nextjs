77f69544392251fe15031954de8d287494d9f354
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-06 00:09:25 +0500

    CHG

--- Hooks/useLoginModel.ts
+++ Hooks/useLoginModel.ts
@@ -7,7 +7,7 @@ interface LoginModelStore {
 }
 
 const useLoganModal = create<LoginModelStore>((set) => ({
-    isOpen:true,
+    isOpen:false,
     onOpen:() => set({isOpen:true}),
     onClose:() => set({isOpen:false})
 }));

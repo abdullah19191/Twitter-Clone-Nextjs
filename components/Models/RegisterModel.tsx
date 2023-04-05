aa4ff78831eaad4cc554b6855b5582f0b0400f64
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-04-05 23:49:25 +0500

    changes

---
+++ components/Models/RegisterModel.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useState } from "react";
+import Input from "../Input";
+import Model from "../Model";
+import useRegisterModel from "../../Hooks/useRegisterModel";
+
+const RegisterModel  = () => {
+  const registerModel = useRegisterModel()
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(  async () => {
+    try {
+      setIsLoading(true);
+      registerModel.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [registerModel]);
+
+   const bodyContent = (
+    <div className=" flex flex-col gap-4">
+        <Input value={email} disabled={isLoading} placeholder="Email" onCharge={(e) => setEmail(e.target.value)}/>
+        <Input value={password} disabled={isLoading} placeholder="Password" onCharge={(e) => setEmail(e.target.value)}/>
+    </div>
+   )
+  return (
+    <Model
+     disabled={isLoading}
+     isOpen={registerModel.isOpen}
+     title="Register"
+     actionLabel="Sign In"
+     onClose={registerModel.onClose}
+     onSubmit={onSubmit}
+     body={bodyContent}
+    />
+  )
+};
+
+export default RegisterModel; 
